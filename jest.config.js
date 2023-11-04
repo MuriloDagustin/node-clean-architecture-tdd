cfa9439e7acdbb87cc17e060e1514bd407e09de0
Murilo_Dagustin <murilo.d.agustin@gmail.com>
2023-11-04 10:57:45 -0400

    add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+/** @type {import('jest').Config} */
+const config = {
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8'
+}
+
+module.exports = config

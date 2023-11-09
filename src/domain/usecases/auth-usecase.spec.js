af5973de5f49c4f68bb80f8648ec7ccf4fede6ba
Murilo_Dagustin <murilo.d.agustin@gmail.com>
2023-11-08 22:41:04 -0400

    Ensure AuthUseCase throws InvalidParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -3,9 +3,13 @@ import { InvalidParamError } from '../../utils/errors'
 
 const makeSut = () => {
   const sut = {
-    auth: jest.fn(async (email) => {
+    auth: jest.fn(async (email, password) => {
       if (!email) {
         throw new InvalidParamError('email')
+      }
+
+      if (!password) {
+        throw new InvalidParamError('password')
       }
 
       return 'valid_acces'
@@ -22,5 +26,11 @@ describe('Auth UseCase', () => {
     const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new InvalidParamError('email'))
+  })
+
+  it('Should throw if no password is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.auth('any_mail@mail.com')
+    expect(promise).rejects.toThrow(new InvalidParamError('password'))
   })
 })
